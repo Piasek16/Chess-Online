92be98463955377d21f53372724cc066901eac1f
Piasek <63462890+Piasek16@users.noreply.github.com>
2022-07-12 20:12:34 +0200

    Add player positioning and name tracking

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -1,18 +1,36 @@
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 
-public class LoginManager : MonoBehaviour
-{
-    void OnGUI() {
-        GUILayout.Label("Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
-        GUILayout.Label("Mode: " + (NetworkManager.Singleton.IsHost ? "Host" : "Not Host (Client obv)"));
-    }
+public class LoginManager : MonoBehaviour {
+
+    string mode = "not connected";
+    public string username = "unnamed";
 
     public void StartHost() {
+        username = transform.GetChild(0).GetComponent<TMP_InputField>().text;
         NetworkManager.Singleton.StartHost();
+        mode = "host";
+        LogConnectionParameters();
+        PutChildrenToSleep();
     }
 
     public void StartClient() {
+        username = transform.GetChild(0).GetComponent<TMP_InputField>().text;
         NetworkManager.Singleton.StartClient();
+        mode = "client";
+        LogConnectionParameters();
+        PutChildrenToSleep();
+    }
+
+    private void LogConnectionParameters() {
+        Debug.Log("Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        Debug.Log("Mode: " + mode);
+    }
+
+    private void PutChildrenToSleep() {
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(2).gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,46 +3,51 @@ using Unity.Netcode;
 using Unity.Collections;
 using TMPro;
 
-public class Player : NetworkBehaviour
-{
+public class Player : NetworkBehaviour {
     public NetworkVariable<FixedString128Bytes> PlayerName = new NetworkVariable<FixedString128Bytes>();
     public NetworkVariable<bool> PlayerColor = new NetworkVariable<bool>();
     private GameObject playerObject;
 
     Vector2 whitePosition = new Vector2(-2f, 1f);
-    Vector2 blackPosition = new Vector2(-2f, 5f);
+    Vector2 blackPosition = new Vector2(-2f, 6f);
 
-    void Start() {
+    void Awake() {
         playerObject = transform.gameObject;
-        SetLocalPlayerName(FindObjectOfType<Canvas>().transform.GetChild(0).GetComponent<TMP_InputField>().text);
     }
 
     void Update() {
-        
+
     }
 
     public override void OnNetworkSpawn() {
         if (IsOwner) SetPlayerColorServerRPC();
-        base.OnNetworkSpawn();
-    }
-
-    [ServerRpc]
-    void SetPlayerColorServerRPC() {
-        PlayerColor.Value = IsHost;
         if (PlayerColor.Value) {
             transform.position = whitePosition;
         } else {
             transform.position = blackPosition;
         }
-    }
-
-    public void SetLocalPlayerName(string name) {
-        if (IsLocalPlayer) {
-            //PlayerName.Value = name;
-            Debug.Log("I am");
-            Debug.Log(name);
+        PlayerName.OnValueChanged += UpdatePlayerObjectName;
+        if (IsOwner) {
+            var _playerName = FindObjectOfType<Canvas>().GetComponent<LoginManager>().username;
+            SetLocalPlayerNameServerRpc(_playerName);
+        } else {
+            playerObject.name = "Player (" + PlayerName.Value + ")";
         }
+        base.OnNetworkSpawn();
     }
 
-   
+    void UpdatePlayerObjectName(FixedString128Bytes previous, FixedString128Bytes newValue) {
+        playerObject.name = "Player (" + PlayerName.Value + ")";
+    }
+
+    [ServerRpc]
+    void SetPlayerColorServerRPC() {
+        PlayerColor.Value = IsHost;
+    }
+
+    [ServerRpc]
+    public void SetLocalPlayerNameServerRpc(string name) {
+        PlayerName.Value = name;
+        Debug.Log("Joined: " + name);
+    }
 }
