c3db99d2d9d25eefac345cfbed224781d7ae8946
Piasek <63462890+Piasek16@users.noreply.github.com>
2022-08-17 14:20:48 +0200

    Add file logging

--- Assets/Scripts/CustomLogger.cs
+++ Assets/Scripts/CustomLogger.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class CustomLogger : MonoBehaviour {
 
     uint qsize = 15;  // number of messages to keep
     Queue myLogQueue = new Queue();
 
+    string logFileName;
+
     void Start() {
         DontDestroyOnLoad(gameObject);
+        logFileName = Application.dataPath + "/ChessOnlineLog.txt";
         Debug.Log("Started up logging.");
     }
 
@@ -20,11 +24,17 @@ public class CustomLogger : MonoBehaviour {
     }
 
     void HandleLog(string logString, string stackTrace, LogType type) {
-        myLogQueue.Enqueue("[" + type + "] : " + logString);
-        if (type == LogType.Exception)
+        TextWriter tw = new StreamWriter(logFileName, true);
+        string line = "[" + type + "] : " + logString;
+        myLogQueue.Enqueue(line);
+        tw.WriteLine("[" + System.DateTime.Now + "] " + line);
+        if (type == LogType.Exception) {
             myLogQueue.Enqueue(stackTrace);
+            tw.WriteLine(stackTrace);
+        }
         while (myLogQueue.Count > qsize)
             myLogQueue.Dequeue();
+        tw.Close();
     }
 
     void OnGUI() {
