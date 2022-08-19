b48f5efc4cbe73e61f6e2213515b563a899be852
Piasek <63462890+Piasek16@users.noreply.github.com>
2022-08-19 23:34:01 +0200

    Fix castling bug, advance version to 0.0.2a

--- Assets/Scripts/Pieces/King.cs
+++ Assets/Scripts/Pieces/King.cs
@@ -20,12 +20,22 @@ public class King : Piece {
         if (!FirstMove) return moves;
         var leftRook = BoardManager.Instance.GetPieceFromSpace(new Vector2Int(0, Position.y)) as Rook;
         var rightRook = BoardManager.Instance.GetPieceFromSpace(new Vector2Int(7, Position.y)) as Rook;
-        if (leftRook != null && leftRook.FirstMove && MoveManager.Instance.IsMoveLegal(Position, new Vector2Int(Position.x - 1, Position.y))) {
+        if (leftRook != null && leftRook.FirstMove 
+            && BoardManager.Instance.GetPieceFromSpace(new Vector2Int(Position.x - 1, Position.y)) == null
+            && MoveManager.Instance.IsMoveLegal(Position, new Vector2Int(Position.x - 1, Position.y))
+            && BoardManager.Instance.GetPieceFromSpace(new Vector2Int(Position.x - 2, Position.y)) == null
+            && MoveManager.Instance.IsMoveLegal(Position, new Vector2Int(Position.x - 2, Position.y))
+            && BoardManager.Instance.GetPieceFromSpace(new Vector2Int(Position.x - 3, Position.y)) == null
+            && MoveManager.Instance.IsMoveLegal(Position, new Vector2Int(Position.x - 3, Position.y))) {
             var _newPosition = new Vector2Int(Position.x - 2, Position.y);
             var _oldPiece = BoardManager.Instance.GetPieceFromSpace(_newPosition);
             if (_oldPiece == null) moves.Add(_newPosition);
         }
-        if (rightRook != null && rightRook.FirstMove && MoveManager.Instance.IsMoveLegal(Position, new Vector2Int(Position.x + 1, Position.y))) {
+        if (rightRook != null && rightRook.FirstMove 
+            && BoardManager.Instance.GetPieceFromSpace(new Vector2Int(Position.x + 1, Position.y)) == null
+            && MoveManager.Instance.IsMoveLegal(Position, new Vector2Int(Position.x + 1, Position.y))
+            && BoardManager.Instance.GetPieceFromSpace(new Vector2Int(Position.x + 2, Position.y)) == null
+            && MoveManager.Instance.IsMoveLegal(Position, new Vector2Int(Position.x + 2, Position.y))) {
             var _newPosition = new Vector2Int(Position.x + 2, Position.y);
             var _oldPiece = BoardManager.Instance.GetPieceFromSpace(_newPosition);
             if (_oldPiece == null) moves.Add(_newPosition);
