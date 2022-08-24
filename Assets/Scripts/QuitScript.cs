80c10c34364a5abcfbe2f193449c9603751bc977
Piasek <63462890+Piasek16@users.noreply.github.com>
2022-08-24 22:16:52 +0200

    Add a closing easter egg

--- Assets/Scripts/QuitScript.cs
+++ Assets/Scripts/QuitScript.cs
@@ -1,7 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class QuitScript : MonoBehaviour {
+
+    public AudioClip clip;
+
     public void ExitGame() {
+        AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
+        StartCoroutine(QuitAfterDelay(clip.length));
+    }
+
+    IEnumerator QuitAfterDelay(float delay) {
+        yield return new WaitForSeconds(delay);
         Application.Quit();
     }
 }
